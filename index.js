bf222c16c1db83104a4206e09f2c955c006f2db0
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-18 04:24:56 +0600

    added booking query for users and date on server site

--- index.js
+++ index.js
@@ -24,9 +24,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const appOptionCollection = client.db('DoctorPortalDB').collection('appointmentOptions');
+    const bookingCollection = client.db('DoctorPortalDB').collection('bookings');
+
+    // Booking Related Api
+    app.post('/bookings', async(req, res) =>{
+      const booking = req.body;
+      // console.log(booking)
+      const query ={
+        selectedDate: booking.selectedDate,
+        email:booking.email,
+        treatment: booking.treatment
+      }
+      const alreadyBooked = await bookingCollection.find(query).toArray();
+
+      if(alreadyBooked.length){
+        const message = `You Already Have a Booking on ${booking.selectedDate}`
+        return res.send({acknowledged: false, message})
+      }
+
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get('/bookings', async(req, res) =>{
+      const query ={};
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+    //Use Aggregate to Query multiple Appointment collection  and then merge Data Related Api
+    app.get('/appointmentOptions', async(req, res)=>{
+      const date = req.query.date;
+       
+        const options = await appOptionCollection.find().toArray();
+        // get the booking of the provided date
+        const bookingQuery = {selectedDate: date}
+        const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+        // code carefully: D
+        options.forEach(option =>{
+          const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+         const bookedSlots = optionBooked.map(book => book.slot);
+
+         const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+         option.slots = remainingSlots;
+
+        //  console.log(date, option.name, remainingSlots.length)
+        })
+
+        res.send(options);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
